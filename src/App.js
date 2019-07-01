92c2ee8879236722b25d4aefbe25ed762fe73e3b
Bailey Kane <bailey.orion.kane@gmail.com>
2019-06-30 23:53:53 -0400

    Remove Amplify stuff?

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-
-Amplify.configure(aws_exports);
 
 function App() {
   return (
