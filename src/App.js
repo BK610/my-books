071a0d94a7acfc1979eb7d827247f9c33450e5cb
Bailey Kane <bailey.orion.kane@gmail.com>
2019-07-01 23:10:59 -0400

    Added bootstrap succesfully

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
 import HomeContainer from "./components/Home/index";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-        <Router>
-          <Route
-            path="/"
-            exact
-            render={(props) => (
-                <HomeContainer {...props}/>
-            )}
-          />
-        </Router>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Router>
+                    <Route
+                        path="/"
+                        exact
+                        render={(props) => (
+                            <HomeContainer {...props}/>
+                        )}
+                    />
+                </Router>
+            </Container>
+        );
+    }
 }
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Button from 'react-bootstrap/Button'
 
 export default function Home(props) {
     return (
         <div>
             <h1>Daily Dad!</h1>
             <p>Coming soon...</p>
-            <a href="https://www.producthunt.com/upcoming/daily-dad">Sign Up Here</a>
+            <a href="https://www.producthunt.com/upcoming/daily-dad">
+                <Button>
+                    Sign Up Here
+                </Button>
+            </a>
         </div>
     )
 }
