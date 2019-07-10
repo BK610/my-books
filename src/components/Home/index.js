9e1ccb3b1118ed7ff139b6530f626c9ec4c91208
Bailey Kane <bailey.orion.kane@gmail.com>
2019-07-10 17:02:43 -0400

    Remove unnecessary update function

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,22 +2,7 @@ import React from "react";
 import Home from "./Home";
 
 export default class HomeContainer extends React.Component {
-    constructor(props) {
-        super(props);
-
-        handleSignupInput: this.handleSignupInput.bind(this);
-    }
-
-    //TODO: Very broken. actually implement
-    handleSignupInput() {
-        console.log("editing input");
-    }
-
     render() {
-        const handlers = {
-            handleSignupInput: this.handleSignupInput
-        };
-
         return <Home/>;
     }
 }
