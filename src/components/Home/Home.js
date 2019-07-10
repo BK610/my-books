231fd799d9e44251e26af7bee663747a6e3ac002
Bailey Kane <bailey.orion.kane@gmail.com>
2019-07-10 17:17:52 -0400

    Formatting

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,17 +8,21 @@ import Col from "react-bootstrap/Col";
 
 export default function Home(props) {
     return (
-        <div>
-            <h1>Daily Dad!</h1>
-            <p>Coming soon...</p>
+        <Row>
+            <h1>
+                Daily Dad!
+            </h1>
+            <p>
+                Coming soon...
+            </p>
             <MailchimpSubscribe url={"https://gmail.us3.list-manage.com/subscribe/post?u=abf60d816e2f94114d163a561&amp;id=22d91fe5ec"}
-            render={({subscribe, status, message}) => (
-                <CustomForm
-                    status={status}
-                    message={message}
-                    onValidated={formData => subscribe(formData)}
-                />
-            )}/>
-        </div>
-)
+                                render={({subscribe, status, message}) => (
+                                    <CustomForm
+                                        status={status}
+                                        message={message}
+                                        onValidated={formData => subscribe(formData)}
+                                    />
+                                )}/>
+        </Row>
+    )
 }
