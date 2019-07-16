54be6f2e9c41523a24ee254833e99faff0c65c91
Bailey Kane <bailey.orion.kane@gmail.com>
2019-07-15 22:25:41 -0400

    Headings work

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,14 @@ export default function Home(props) {
     return (
         <section>
             <Jumbotron>
-                Big Jumbo
+                <h1>
+                    Big Jumbo
+                </h1>
+                <h2>Heading 2</h2>
+                <h3>Heading 3</h3>
+                <h4>Heading 4</h4>
+                <h5>Heading 5</h5>
+                <h6>Heading 6</h6>
             </Jumbotron>
             <Row id={'rowDailyDad'}>
                 <Col></Col>
