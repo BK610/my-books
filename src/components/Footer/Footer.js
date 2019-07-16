abaae2bba75df2fbb330356df6ff40be4a57c9e1
Bailey Kane <bailey.orion.kane@gmail.com>
2019-07-16 14:20:05 -0400

    Created Footer with basic message

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import {styles} from './styles';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Footer(props) {
     return (
-        <section>
-            Footer!
+        <section style={styles.FooterSection}>
+            <Container style={styles.Container}>
+                <Row style={styles.Row}>
+                    <Col>
+                        <p style={styles.Text}>
+                            Made with&nbsp;❤&nbsp;by&nbsp;
+                            <strong>
+                                <a style={styles.Link}
+                                   href={'https://www.linkedin.com/in/minaiskarous/'}
+                                   target={'_blank'}>
+                                    Mina
+                                </a>
+                            </strong>
+                            &nbsp;and&nbsp;
+                            <strong>
+                                <a style={styles.Link}
+                                   href={'https://www.linkedin.com/in/baileykane/'}
+                                   target={'_blank'}>
+                                    Bailey
+                                </a>
+                            </strong>
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
         </section>
     )
 }
