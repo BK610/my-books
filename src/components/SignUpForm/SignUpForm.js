2db11b598bd2f579729321965943ba4ab90723a6
Bailey Kane <bailey.orion.kane@gmail.com>
2019-08-11 18:48:53 -0400

    Refactored as a class, removed bootstrap, formatting multiple ways

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,105 +1,131 @@
 import React from 'react';
-import {styles} from './styles';
 import {
     Button,
     Input,
     Icon,
     Row,
-    Col
+    Col,
+    Text
 } from 'atomize';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
 
-const SignUpForm = ({status, message, onValidated}) => {
-    let email;
+class SignUpForm extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props);
 
-    const submit = event => {
+        this.state = {
+            email: ""
+        };
+
+        this.submit = this.submit.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
+    }
+
+    submit = event => {
         event.preventDefault();
-        email &&
-        onValidated({
-            EMAIL: email
+        this.state.email &&
+        this.props.onValidated({
+            EMAIL: this.state.email
         });
-        console.log(email);
+        console.log(this.state.email);
     };
 
-    const handleEmail = event => {
-        email = event.target.value;
+    handleEmail = event => {
+        this.setState({email: event.target.value})
     };
 
-    return (
-        <div>
-            <form onSubmit={submit}>
-                <Row>
-                    <Col size={{
-                        xs: 0,
-                        sm: 2,
-                        lg: 3
-                    }}
-                    ></Col>
-                    <Col size={{
-                        xs: 12,
-                        sm: 4,
-                        lg: 3
-                    }}
-                    >
-                        <Input
-                            bg="inputGray"
-                            shadow="2"
-                            focusShadow="3"
-                            type="email"
-                            name="email"
-                            value={email}
-                            onChange={handleEmail}
-                            placeholder="Your email here"
-                            suffix={
-                                <Icon
-                                    name="RightArrow"
-                                    pos="absolute"
-                                    right="1rem"
-                                    top="50%"
-                                    transform="translateY(-50%)"
-                                />
-                            }
-                        />
-                    </Col>
-                    <Col size={{
-                        xs: 12,
-                        sm: 3
-                    }}
-                    >
-                        <Button
-                            bg="brandTeal"
-                            shadow="2"
-                            hoverShadow="3"
+    render() {
+
+        return (
+            <div>
+                <form onSubmit={this.submit}>
+                    <Row>
+                        <Col size={{
+                            xs: 0,
+                            sm: 2,
+                            lg: 3
+                        }}
+                        ></Col>
+                        <Col size={{
+                            xs: 12,
+                            sm: 4,
+                            lg: 3
+                        }}
                         >
-                            Submit
-                        </Button>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col size={{
-                        xs: 0,
-                        sm: 2,
-                        lg: 3
-                    }}
-                    ></Col>
-                    <Col size={{
-                        xs: 12,
-                        sm: 8,
-                        lg: 6
-                    }}
-                    >
-                        {status === "error" && (
-                            <div dangerouslySetInnerHTML={{__html: message}}/>
-                        )}
-                        {status === "success" && (
-                            <div dangerouslySetInnerHTML={{__html: message}}/>
-                        )}
-                    </Col>
-                </Row>
-            </form>
-        </div>
-    );
+                            <Input
+                                bg="inputGray"
+                                shadow="2"
+                                hoverShadow="3"
+                                focusShadow="3"
+                                type="email"
+                                name="email"
+                                value={this.state.email}
+                                onChange={this.handleEmail}
+                                placeholder="Your email here"
+                                textColor={"brandNavy"}
+                                suffix={
+                                    <Icon
+                                        name="RightArrow"
+                                        color={"brandNavy"}
+                                        pos="absolute"
+                                        right="1rem"
+                                        top="50%"
+                                        transform="translateY(-50%)"
+                                        cursor={"pointer"}
+                                        onClick={this.submit}
+                                    />
+                                }
+                            />
+                        </Col>
+                        <Col size={{
+                            xs: 12,
+                            sm: 3
+                        }}
+                        >
+                            {/*<Button*/}
+                            {/*    bg="brandTeal"*/}
+                            {/*    hoverBg="brandTeal"*/}
+                            {/*    shadow="2"*/}
+                            {/*    hoverShadow="3"*/}
+                            {/*    rounded="circle"*/}
+                            {/*    m={{r: "1rem"}}*/}
+                            {/*    h={"2.5rem"}*/}
+                            {/*    w={"2.5rem"}*/}
+                            {/*>*/}
+                            {/*    <Icon name={"Email"}*/}
+                            {/*    color={"white"}/>*/}
+                            {/*    /!*<Text style={{fontSize: "16px"}}>*!/*/}
+                            {/*    /!*    Submit*!/*/}
+                            {/*    /!*</Text>*!/*/}
+                            {/*</Button>*/}
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col size={{
+                            xs: 0,
+                            sm: 2,
+                            lg: 3
+                        }}
+                        ></Col>
+                        <Col size={{
+                            xs: 12,
+                            sm: 8,
+                            lg: 6
+                        }}
+                        >
+                            <br/>
+                            {this.props.status === "error" && (
+                                <div dangerouslySetInnerHTML={{__html: this.props.message}}/>
+                            )}
+                            {this.props.status === "success" && (
+                                <div dangerouslySetInnerHTML={{__html: this.props.message}}/>
+                            )}
+                        </Col>
+                    </Row>
+                </form>
+            </div>
+        );
+    }
 };
 
 export default SignUpForm;
