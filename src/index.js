19e9fd115c32e984f1e942b6f1f6f33cacab042c
Bailey Kane <bailey.orion.kane@gmail.com>
2019-08-11 18:51:12 -0400

    Add base atomize package information, adjust theme with brand colors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {ThemeProvider,
-    StyleReset} from 'atomize';
+import {
+    ThemeProvider,
+    StyleReset
+} from 'atomize';
 import {theme} from './theme';
-// import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,10 @@ import {DefaultTheme} from 'atomize';
 
 export const theme = {
     ...DefaultTheme,
+    fontFamily: {
+        ...DefaultTheme.fontFamily,
+        primary: "Cooper-Hewitt"
+    },
     colors: {
         ...DefaultTheme.colors,
         inputGray: "#E9ECEF",
