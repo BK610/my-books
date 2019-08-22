9f24d2a451b0c34c9d6a75944a4baecf9f00fb13
Bailey Kane <bailey.orion.kane@gmail.com>
2019-08-22 00:45:00 -0400

    Added service to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import GoogleBooksService from './services/GoogleBooksService';
 import NavBarContainer from "./components/NavBar/index";
 import MainContainer from "./components/Main";
 import FooterContainer from "./components/Footer";
@@ -7,6 +8,8 @@ import FooterContainer from "./components/Footer";
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+        this.googleBooksService = GoogleBooksService.getInstance();
     }
 
     render() {
